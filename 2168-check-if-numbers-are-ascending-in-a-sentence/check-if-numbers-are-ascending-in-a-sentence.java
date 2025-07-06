764633e9e0ecb054572785355579d5645f3a5890
Preksha Sangal <prekshasangal@gmail.com>
2025-07-06 17:41:03 +0530

    Time: 1 ms (89.75%) | Memory: 41.4 MB (89.34%) - LeetSync

--- 2168-check-if-numbers-are-ascending-in-a-sentence/check-if-numbers-are-ascending-in-a-sentence.java
+++ 2168-check-if-numbers-are-ascending-in-a-sentence/check-if-numbers-are-ascending-in-a-sentence.java
@@ -2,9 +2,9 @@ class Solution {
     public boolean areNumbersAscending(String s) {
         String[] words = s.split(" ");
         int prev = -1;
-        for (String word : words) {
-            if (Character.isDigit(word.charAt(0))) {
-                int num = Integer.parseInt(word);
+        for (int i=0;i<words.length;i++) {
+            if (Character.isDigit(words[i].charAt(0))) {
+                int num = Integer.parseInt(words[i]);
                 if (num <= prev) {
                     return false;
                 }
