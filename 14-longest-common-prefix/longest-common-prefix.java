2b96d3a2fe3eac5245aa75fc008f0f21c7d4c0ee
Preksha Sangal <prekshasangal@gmail.com>
2025-07-09 17:46:41 +0530

    Time: 1 ms (63.61%) | Memory: 41.6 MB (63.42%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -7,7 +7,7 @@ class Solution {
         for(int i=0;i<first.length();i++){
             char ch=first.charAt(i);
 
-        for(int j=0;j<strs.length;j++){
+        for(int j=1;j<strs.length;j++){
             if(i>= strs[j].length() || strs[j].charAt(i) != ch){
                 return first.substring(0,i);
             }
